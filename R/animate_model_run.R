421c987973df2da199c6e43708f8ed37cf67c4eb
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-06-13 23:51:09 +0200

    Added some documentation
    * also added default color scheme for up to 9 states

--- R/animate_model_run.R
+++ R/animate_model_run.R
@@ -1,13 +1,38 @@
 #' Animate a model run
 #'
-#' @param df The simulation data.
-#' @param marker_size Size of the markers.
-#' @param color_scheme Which colors to use for which cell state.
+#' `animate_model_run` takes the results of an automaton run and animates it
+#' leveraging the `gganimate` library.
+#' You can specify the marker size on the grid and customize the color scheme.
 #'
-#' @return A gganimate animation
+#' @param df The simulation data as returned by the `run_automaton` function.
+#' @param marker_size Size of the cells on the grid
+#'   (needs adjustment depending on grid size).
+#' @param color_scheme Which colors to use for which cell state. As the cell
+#'   states are defined by a sequence of integers starting at 0, you can simply
+#'   specify the colors by providing a vector with each entry `i` representing
+#'   the color for state `i`.
+#'
+#' @return A gganimate animation.
 #' @export
 #'
 #' @examples
+#' initial_state <- matrix(
+#'   sample(
+#'     c(0, 1, 2, 3),
+#'     replace = TRUE,
+#'     prob = c(0.25, 0.25, 0.25, 0.25),
+#'     400
+#'   ),
+#'   nrow = 20, ncol = 20
+#' )
+#'
+#' results <- run_automaton(initial_state, 100, schelling_step, tolerance = 3)
+#'
+#' animate_model_run(
+#'   results,
+#'   5,
+#'   c("transparent", "gold2", "royalblue", "coral")
+#' )
 animate_model_run <- function(df, marker_size, color_scheme) {
   anim <- df %>%
     plot_state(marker_size, color_scheme) +
--- R/brians_brain.R
+++ R/brians_brain.R
@@ -1,11 +1,29 @@
-#' One step in Brian's brain
+#' One step in the Brian's Brain automaton
 #'
-#' @param m The current state.
+#' Brian's Brain is an automaton with three distinct states that produces some
+#' characteristic chaotic patterns.
+#' The function is usually given as the `stepfunc` argument to `run_automaton`.
+#' Check out <https://en.wikipedia.org/wiki/Brian's_Brain> for more information.
 #'
-#' @return A matrix representing the subsequent state
+#' @param m The current state of the automaton. This matrix should have three
+#'   distinct states: 0, 1, and 2.
+#'
+#' @return A matrix representing the subsequent state.
 #' @export
 #'
 #' @examples
+#' initial_state <- matrix(
+#'   sample(c(0, 1, 2), replace = TRUE, prob = c(0.5, 0.25, 0.25), 200),
+#'   nrow = 20, ncol = 20
+#' )
+#'
+#' # Visualization of the initial state
+#' plot_state(initial_state, 5, theme_brians_brain_light())
+#'
+#' next_state <- brians_brain_step(initial_state)
+#'
+#' # Visualization of the subsequent state
+#' plot_state(next_state, 5, theme_brians_brain_light())
 brians_brain_step <- function(m) {
   axis_size <- dim(m)[1]
   m_upd <- matrix(nrow = axis_size, ncol = axis_size, rep(0, axis_size^2))
@@ -35,19 +53,3 @@ brians_brain_step <- function(m) {
 
   return(m_upd)
 }
-
-# brians_brain_game <- function(initial_state, steps) {
-#
-#   m <- initial_state
-#   d <- board_to_df(m)
-#   d$step <- 0
-#   for (i in 1:steps) {
-#     m <- brians_brain_step(m)
-#     d_step <- board_to_df(m)
-#     d_step$step <- i
-#     d <- dplyr::bind_rows(d, d_step)
-#   }
-#   d <- dplyr::select(d, step, cell_id, row, col, state)
-#
-#   return(d)
-# }
