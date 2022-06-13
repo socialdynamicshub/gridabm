c4baa5fcf91070ce16f48d5f24633c1e2482b374
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-06-13 11:01:27 +0200

    Added theming functions

---
+++ R/themes.R
@@ -0,0 +1,50 @@
+#' Theme for the Forest Fire automaton
+#'
+#' @return A vector containing the color scheme
+#' @export
+#'
+#' @examples
+theme_forest_fire <- function() {
+  return(c("white", "green", "orange", "firebrick"))
+}
+
+
+#' Theme for the Schelling automaton
+#'
+#' @return A vector containing the color scheme
+#' @export
+#'
+#' @examples
+theme_schelling <- function() {
+  return(c("white", "orange", "darkblue"))
+}
+
+#' Theme for the Life automaton
+#'
+#' @return A vector containing the color scheme
+#' @export
+#'
+#' @examples
+theme_life <- function() {
+  return(c("white", "black"))
+}
+
+#' Dark theme for the Brian's Brain automaton
+#'
+#' @return A vector containing the color scheme
+#' @export
+#'
+#' @examples
+theme_brians_brain_dark <- function() {
+  return(c("black", "white", "blue"))
+}
+
+#' Light theme for the Brian's Brain automaton
+#'
+#' @return A vector containing the color scheme
+#' @export
+#'
+#' @examples
+theme_brians_brain_light <- function() {
+  return(c("white", "black", "blue"))
+}
