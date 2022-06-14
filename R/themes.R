2c4863bd2a62585bcf378c8f072ae247c42b40d4
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-06-14 16:00:45 +0200

    Naive implementation of Langton's ant
    * fixed theme bug
    * updated some documentation

--- R/themes.R
+++ R/themes.R
@@ -4,8 +4,16 @@
 #' @export
 #'
 #' @examples
+#' theme_forest_fire()
 theme_forest_fire <- function() {
-  return(c("transparent", "green", "orange", "firebrick"))
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "green",
+      `2` = "orange",
+      `3` = "firebrick"
+    )
+  )
 }
 
 
@@ -15,8 +23,15 @@ theme_forest_fire <- function() {
 #' @export
 #'
 #' @examples
+#' theme_schelling()
 theme_schelling <- function() {
-  return(c("transparent", "orange", "darkblue"))
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "orange",
+      `2` = "darkblue"
+    )
+  )
 }
 
 #' Theme for the Life automaton
@@ -25,8 +40,14 @@ theme_schelling <- function() {
 #' @export
 #'
 #' @examples
+#' theme_life()
 theme_life <- function() {
-  return(c("transparent", "black"))
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "black"
+    )
+  )
 }
 
 #' Dark theme for the Brian's Brain automaton
@@ -35,8 +56,15 @@ theme_life <- function() {
 #' @export
 #'
 #' @examples
+#' theme_brians_brain_dark()
 theme_brians_brain_dark <- function() {
-  return(c("transparent", "white", "blue"))
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "white",
+      `2` = "blue"
+    )
+  )
 }
 
 #' Light theme for the Brian's Brain automaton
@@ -45,9 +73,42 @@ theme_brians_brain_dark <- function() {
 #' @export
 #'
 #' @examples
+#' theme_brians_brain_light()
 theme_brians_brain_light <- function() {
-  return(c("transparent", "black", "blue"))
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "black",
+      `2` = "blue"
+    )
+  )
 }
+
+
+#' Theme for Langton's Ant
+#'
+#' @return A vector containing the color scheme.
+#' @export
+#'
+#' @examples
+#' theme_langtons_ant()
+theme_langtons_ant <- function() {
+  return(
+    c(
+      `0` = "transparent",
+      `1` = "black",
+      `2` = "red",
+      `3` = "red",
+      `4` = "red",
+      `5` = "red",
+      `6` = "red",
+      `7` = "red",
+      `8` = "red",
+      `9` = "red"
+    )
+  )
+}
+
 
 #' Default theme if no other theme was supplied
 #'
@@ -55,11 +116,20 @@ theme_brians_brain_light <- function() {
 #' @export
 #'
 #' @examples
+#' theme_default()
 theme_default <- function() {
+  pal <- RColorBrewer::brewer.pal(name = "Dark2", n = 8)
   return(
     c(
-      "transparent",
-      RColorBrewer::brewer.pal(name = "Dark2", n = 8)
+      `0` = "transparent",
+      `1` = pal[1],
+      `2` = pal[2],
+      `3` = pal[3],
+      `4` = pal[4],
+      `5` = pal[5],
+      `6` = pal[6],
+      `7` = pal[7],
+      `8` = pal[8],
     )
   )
 }
