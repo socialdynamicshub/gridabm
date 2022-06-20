b161035a3086c1760091cb39aac0cbb70263bee7
JohannesNakayama <38665199+JohannesNakayama@users.noreply.github.com>
2022-06-20 23:40:38 +0200

    Initialized tests and wrote some
    * tests for get_moore_neighborhood

--- tests/testthat/test-get_moore_neighborhood.R
+++ tests/testthat/test-get_moore_neighborhood.R
@@ -1,3 +1,55 @@
-test_that("get_moore_neighborhood works in the middle of the grid", {
-  expect_equal(2 * 2, 4)  # TODO: write actual test
+test_that("get_moore_neighborhood works in the middle", {
+  expect_equal(
+    get_moore_neighborhood(5, 5, c(10, 10)),
+    list(
+      c(4, 4), c(4, 5), c(4, 6),
+      c(5, 4), c(5, 6),
+      c(6, 4), c(6, 5), c(6, 6)
+    )
+  )
 })
+
+test_that("get_moore_neighborhood works at the edge", {
+  expect_equal(
+    get_moore_neighborhood(1, 5, c(10, 10)),
+    list(
+      c(10, 4), c(10, 5), c(10, 6),
+      c(1, 4), c(1, 6),
+      c(2, 4), c(2, 5), c(2, 6)
+    )
+  )
+})
+
+test_that("get_moore_neighborhood works in the corner", {
+  expect_equal(
+    get_moore_neighborhood(1, 1, c(10, 10)),
+    list(
+      c(10, 10), c(10, 1), c(10, 2),
+      c(1, 10), c(1, 2),
+      c(2, 10), c(2, 1), c(2, 2)
+    )
+  )
+})
+
+test_that("non-periodic get_moore_neighborhood works at the edge", {
+  expect_equal(
+    get_moore_neighborhood(1, 5, c(10, 10), periodic = FALSE),
+    list(
+      c(1, 4), c(1, 6),
+      c(2, 4), c(2, 5), c(2, 6)
+    )
+  )
+})
+
+test_that("non-periodic get_moore_neighborhood works in the corner", {
+  expect_equal(
+    get_moore_neighborhood(1, 1, c(10, 10), periodic = FALSE),
+    list(
+      c(1, 2),
+      c(2, 1), c(2, 2)
+    )
+  )
+})
+
+
+
